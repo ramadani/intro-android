c06fd525a54c2c322a83d4d5836a92fc392fe084
Ramadani <email.ramadani@gmail.com>
2016-11-03 16:44:13 +0700

    book list

---
+++ app/src/main/java/com/example/dani/introandroiddemo/BookListActivity.java
@@ -0,0 +1,118 @@
+package com.example.dani.introandroiddemo;
+
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.example.dani.introandroiddemo.book.Book;
+import com.example.dani.introandroiddemo.book.BookAdapter;
+import com.example.dani.introandroiddemo.book.BookClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+import static android.R.attr.id;
+import static com.example.dani.introandroiddemo.R.string.search;
+
+public class BookListActivity extends AppCompatActivity {
+    private ListView lvBooks;
+    private BookAdapter bookAdapter;
+
+    private BookClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_list);
+
+        lvBooks = (ListView) findViewById(R.id.lvBooks);
+        ArrayList<Book> books = new ArrayList<Book>();
+        bookAdapter = new BookAdapter(this, books);
+        lvBooks.setAdapter(bookAdapter);
+
+        // Fetch the data remotely
+        // fetchBooks();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_book_list, menu);
+
+        final MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Fetch the data remotely
+                fetchBooks(query);
+
+                // Reset SearchView
+                searchView.clearFocus();
+                searchView.setQuery("", false);
+                searchView.setIconified(true);
+
+                searchItem.collapseActionView();
+
+                // Set activity title to search query
+                BookListActivity.this.setTitle(query);
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        return true;
+    }
+
+    // Executes an API call to the OpenLibrary search endpoint, parses the results
+    // Converts them into an array of book objects and adds them to the adapter
+    private void fetchBooks(String query) {
+        client = new BookClient();
+
+        client.getBooks(query, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    JSONArray docs = null;
+
+                    if (response != null) {
+                        // Get the docs json array
+                        docs = response.getJSONArray("docs");
+
+                        // Parse json array into array of model objects
+                        final ArrayList<Book> books = Book.fromJson(docs);
+
+                        // Remove all books from the adapter
+                        bookAdapter.clear();
+
+                        // Load model objects into the adapter
+                        for (Book book : books) {
+                            bookAdapter.add(book);
+                        }
+
+                        bookAdapter.notifyDataSetChanged();
+                    }
+                } catch (JSONException e) {
+                    // Invalid JSON format, show appropriate error.
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/dani/introandroiddemo/book/Book.java
@@ -0,0 +1,105 @@
+package com.example.dani.introandroiddemo.book;
+
+import android.text.TextUtils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class Book {
+    private String openLibraryId;
+    private String author;
+    private String title;
+
+    public String getOpenLibraryId() {
+        return openLibraryId;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCoverUrl() {
+        return "http://covers.openlibrary.org/b/olid/" + openLibraryId + "-M.jpg?default=false";
+    }
+
+    public String getLargeCoverUrl() {
+        return "http://covers.openlibrary.org/b/olid/" + openLibraryId + "-L.jpg?default=false";
+    }
+
+    // return a book given the expected json
+    public static Book fromJson(JSONObject jsonObject) {
+        Book book = new Book();
+
+        try {
+            // deserialize json into object fields
+            // check if a cover ediion is available
+            if (jsonObject.has("cover_edition_key")) {
+                book.openLibraryId = jsonObject.getString("cover_edition_key");
+            } else if (jsonObject.has("edition_key")) {
+                final JSONArray ids = jsonObject.getJSONArray("edition_key");
+                book.openLibraryId = ids.getString(0);
+            }
+
+            book.title = jsonObject.has("title_suggest") ? jsonObject.getString("title_suggest") : "";
+            book.author = getAuthor(jsonObject);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        // return new object
+        return book;
+    }
+
+    // Decodes array of book json results into business model objects
+    public static ArrayList<Book> fromJson(JSONArray jsonArray) {
+        ArrayList<Book> books = new ArrayList<Book>(jsonArray.length());
+
+        // Process each result in json array, decode and convert to business
+        // object
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject bookJson = null;
+
+            try {
+                bookJson = jsonArray.getJSONObject(i);
+            } catch (Exception e) {
+                e.printStackTrace();
+                continue;
+            }
+
+            Book book = Book.fromJson(bookJson);
+
+            if (book != null) {
+                books.add(book);
+            }
+        }
+
+        return books;
+    }
+
+    // Return comma separated author list when there is more than one author
+    private static String getAuthor(final JSONObject jsonObject) {
+        try {
+            final JSONArray authors = jsonObject.getJSONArray("author_name");
+            int numAuthors = authors.length();
+
+            final String[] authorStrings = new String[numAuthors];
+
+            for (int i = 0; i < numAuthors; ++i) {
+                authorStrings[i] = authors.getString(i);
+            }
+
+            return TextUtils.join(", ", authorStrings);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+}
